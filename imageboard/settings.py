efcb9c9fd1ef777fdc06858155f38c10c7f1fbf9
b0rgib <vs.bor@yandex.ru>
2022-11-23 21:00:23 +0500

    Update settings.py

--- imageboard/settings.py
+++ imageboard/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "..", "www", "media")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Default primary key field type
