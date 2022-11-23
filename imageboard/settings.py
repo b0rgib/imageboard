55d8fc5119b45e65d29af298efbf43a53b06b1ce
b0rgib <vs.bor@yandex.ru>
2022-11-23 20:25:47 +0500

    54654

--- imageboard/settings.py
+++ imageboard/settings.py
@@ -21,13 +21,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-su8oy^pl@)bc=msp!j-7r5-eg2syfkc8h0d!)+ry!iizpxbwx7'
+SECRET_KEY = 'su8oy^pl@)bc=msp!j-7r5-eg2syfkc8h0d!)+ry!iizpxbwx7oherwigu78hmlawudfwhfuiweyf783y2a;97wwefh8yg76tcusgw7egcewuhf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+SECURE_HSTS_SECONDS = 3600
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_HSTS_PRELOAD = True
 # Application definition
 
 INSTALLED_APPS = [
